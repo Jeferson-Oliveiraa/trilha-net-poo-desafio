ee7d56afecaf6026a7e3be405d7b66d6dac10b61
Jeferson <jeferson143@hotmail.com>
2024-04-27 11:37:37 -0300

    Atualizando código

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,15 +1,25 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
 
-        public Smartphone(string numero)
+        private string IMEI { get; set; }
+
+        private int Memoria { get; set; }
+        
+        public Smartphone(string numero, string modelo, string Imei,int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = Imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
-        }
+        } 
 
         public void Ligar()
         {
