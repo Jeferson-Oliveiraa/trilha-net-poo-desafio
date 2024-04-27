856210f69f96ae68974c78fe208eb4000878da14
Jeferson <jeferson143@hotmail.com>
2024-04-27 15:47:29 -0300

    Atualizando código

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,14 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // TODO: Sobrescrever o método "InstalarAplicativo
+        public Iphone (String numero, string modelo, string Imei, int memoria) : base (numero, modelo, Imei, memoria){}
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no iPhone");
+        }
     }
-}
+
+    }
