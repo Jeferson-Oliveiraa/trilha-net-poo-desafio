cb1ab996d584f5b9ee03c0dfd0c5052bebede2af
Jeferson <jeferson143@hotmail.com>
2024-04-27 15:31:03 -0300

    Atualizando código

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia 
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // TODO: Sobrescrever o método "InstalarAplicativo";
+        public Nokia(string numero, string modelo, string Imei, int memoria) : base(numero, modelo, Imei, memoria)
+        {
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia");
+        }
     }
 }
